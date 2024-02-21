85f139112e2234f911526633b2161421dc4984c3
eddoog <alfredo.austin@ui.ac.id>
2024-02-22 02:01:20 +0700

    feat: add swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+
+  app.enableCors({ origin: '*' });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove non-whitelisted properties
+    }),
+  );
+
+  console.log('Listening on Port ' + (process.env.PORT || 3001));
+
+  const config = new DocumentBuilder()
+    .setTitle('Questify API')
+    .setDescription('Questify PPL API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
