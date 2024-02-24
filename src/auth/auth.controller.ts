1c01e1ad41047654bab5b5f26ccb4feb31b901d2
eddoog <alfredo.austin@ui.ac.id>
2024-02-25 03:48:59 +0700

    feat: init controller and service auth feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDTO, RegisterDTO, SSOAuthDTO } from 'src/dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/login')
+  async login(@Body() loginDTO: LoginDTO) {
+    return this.authService.login(loginDTO);
+  }
+
+  @Post('/register')
+  async register(@Body() registerDTO: RegisterDTO) {
+    return this.authService.register(registerDTO);
+  }
+
+  @Post('/login-sso')
+  async loginSSO(@Body() ssoDTO: SSOAuthDTO) {
+    return this.authService.loginSSO(ssoDTO);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDTO, RegisterDTO, SSOAuthDTO } from 'src/dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prismaService: PrismaService,
+    private readonly httpService: HttpService,
+  ) {}
+
+  async login(loginDTO: LoginDTO) {
+    return loginDTO;
+  }
+
+  async register(registerDTO: RegisterDTO) {
+    return registerDTO;
+  }
+
+  async loginSSO(ssoDTO: SSOAuthDTO) {
+    return ssoDTO;
+  }
+}
