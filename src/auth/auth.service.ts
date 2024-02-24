7396104d10a8bc59be9326dda1a83dcdbf7afb9b
eddoog <alfredo.austin@ui.ac.id>
2024-02-25 03:51:21 +0700

    feat: add login feature based on test

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDTO, RegisterDTO, SSOAuthDTO } from 'src/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -13,11 +18,64 @@ export class AuthService {
   ) {}
 
   async login(loginDTO: LoginDTO) {
-    return loginDTO;
+    const { email, password } = loginDTO;
+
+    if (await this.validateUIEmail(email)) {
+      throw new BadRequestException('Please sign in with SSO instead');
+    }
+
+    const user = await this.findUserByEmail(email);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new BadRequestException('Password does not match');
+    }
+
+    const token = await this.signToken(user.id);
+
+    return {
+      statusCode: 200,
+      message: 'Success',
+      data: {
+        accessToken: token,
+      },
+    };
   }
 
   async register(registerDTO: RegisterDTO) {
     return registerDTO;
+  }
+
+  private async validateUIEmail(email: string) {
+    return email.endsWith('@ui.ac.id');
+  }
+
+  private async findUserByEmail(email: string) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  }
+
+  private async signToken(userId: string) {
+    const payload = {
+      userId,
+    };
+
+    const token = await this.jwtService.signAsync(payload, {
+      expiresIn: '60m',
+      secret: 'TEST_SECRET_KEY',
+    });
+
+    return token;
   }
 
   async loginSSO(ssoDTO: SSOAuthDTO) {
