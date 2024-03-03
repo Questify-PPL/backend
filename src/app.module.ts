d29e4c1012bc3bb38f3ef906d4df7d06c186084b
eddoog <alfredo.austin@ui.ac.id>
2024-03-04 02:17:05 +0700

    chores: init form module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { EmailModule } from './email/email.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { FormModule } from './form/form.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UserModule } from './user/user.module';
     PrismaModule,
     UserModule,
     EmailModule,
+    FormModule,
   ],
   controllers: [AppController],
   providers: [AppService],
