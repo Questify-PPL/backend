dcf06b9b3fcb2a8d0ae9ff08ea85d9fee322b6b3
Achieva Futura Gemilang <achieva.futura@ui.ac.id>
2024-03-30 23:36:28 +0700

    Resolve migration error

--- prisma/migrations/20240330163038_add_refactored_invoice_topup_model/migration.sql
+++ prisma/migrations/20240330163038_add_refactored_invoice_topup_model/migration.sql
@@ -1,12 +1,19 @@
+-- CreateEnum
+CREATE TYPE "InvoiceStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED');
+
 -- CreateTable
 CREATE TABLE "InvoiceTopup" (
     "id" TEXT NOT NULL,
     "creatorId" TEXT NOT NULL,
+    "creatorName" TEXT NOT NULL,
     "amount" INTEGER NOT NULL,
-    "status" TEXT NOT NULL,
+    "status" "InvoiceStatus" NOT NULL DEFAULT 'PENDING',
     "buktiPembayaranUrl" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "validatedAt" TIMESTAMP(3),
+    "payment" TEXT NOT NULL,
+    "exchange" TEXT NOT NULL,
+    "accountNumber" TEXT,
 
     CONSTRAINT "InvoiceTopup_pkey" PRIMARY KEY ("id")
 );
