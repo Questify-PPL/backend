1f3f58a93d5362eddc6e55f84f4b6f26ab229f17
eddoog <codewithalfredo@gmail.com>
2024-05-14 21:26:51 +0700

    chores: migrate vms to sg to reduce network overhead

--- src/tracer.ts
+++ src/tracer.ts
@@ -9,7 +9,7 @@ import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 // Configure the SDK to export telemetry data to the console
 // Enable all auto-instrumentations from the meta package
 const exporterOptions = {
-  url: 'http://34.68.24.231:4318/v1/traces',
+  url: 'http://34.87.24.210:4318/v1/traces',
 };
 
 const traceExporter = new OTLPTraceExporter(exporterOptions);
